feb2f47d68615923a0de99a1057b8344c7bc4778
Marcelo A Macagnan <marcelo.a.mac@gmail.com>
2022-04-22 11:44:31 -0300

    20220422

--- src/modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController.ts
+++ src/modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController.ts
@@ -2,11 +2,15 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import { ResetPasswordUserUseCase } from './ResetPasswordUserUseCase';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { SimpleConsoleLogger } from 'typeorm';
 
 class ResetPasswordUserController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { token } = request.query;
     const { password } = request.body;
+
+    console.log('query:');
+    console.log(request.query);
 
     const resetPasswordUserUseCase = container.resolve(
       ResetPasswordUserUseCase
