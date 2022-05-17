8ba2d5ee7e5648b7a08cbe5eb06fb45bfac7e2cf
Marcelo A. Macagnan <marcelo.a.mac@gmail.com>
2022-05-17 15:29:10 -0300

    20220517

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,35 +1,3 @@
-import { container } from 'tsyringe';
-import { IDateProvider } from './DateProvider/IDateProvider';
-import { DayjsDateProvider } from './DateProvider/implementations/DayjsDateProvider';
-import { IMailProvider } from './MailProvider/IMailProvider';
-import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider';
-import { S3StorageProvider } from './StorageProvider/implementations/S3StorageProvider';
-import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider';
-import { IStorageProvider } from './StorageProvider/IStorageProvider';
-
-container.registerSingleton<IDateProvider>(
-  'DayjsDateProvider',
-  DayjsDateProvider
-);
-
-/**
- * Este provider precisa ser injetado assim que a aplicação é iniciada para que seja possível
- * criar o "client" (no EtherealMailProvider) antes de chamar o método "sendMail()". Para isso
- * use-se o "registerInstance".
- * Obs: Apesar de não ser um registerSingleton, o container também será inicializado somente
- * uma vez.
- */
-container.registerInstance<IMailProvider>(
-  'EtherealMailProvider',
-  new EtherealMailProvider()
-);
-
-const diskStorage = {
-  local: LocalStorageProvider,
-  s3: S3StorageProvider
-}
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  diskStorage[process.env.disk]
-);
+import './DateProvider';
+import './MailProvider';
+import './StorageProvider';
