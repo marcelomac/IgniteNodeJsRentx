520c0fdd244c5e2f6513358e0f78db517cbe1301
Marcelo A Macagnan <marcelo.a.mac@gmail.com>
2022-05-19 11:14:38 -0300

    20220519

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,24 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: {node: 'current'}}],
-    '@babel/preset-typescript'
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
   ],
   plugins: [
     [
       'module-resolver',
       {
-        alias: { 
-          "@modules": ["./src/modules"],
-          "@config": ["./src/config"],
-          "@shared": ["./src/shared"],
-          "@utils": ["./src/utils"],
-          "@errors": ["./src/errors"]
-        }
-      }
+        alias: {
+          '@modules': './src/modules',
+          '@config': './src/config',
+          '@shared': './src/shared',
+          '@utils': './src/utils',
+          '@errors': './src/errors',
+        },
+      },
     ],
     'babel-plugin-transform-typescript-metadata',
-    ['@babel/plugin-proposal-decorators', {legacy: true}],
-    ['@babel/plugin-proposal-class-properties', {loose: true}],
-    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
-  ]
-}
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+  ],
+};
