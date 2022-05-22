84e72d38688aac0f75ee6d5a2fe722f18c45a876
Marcelo A Macagnan <marcelo.a.mac@gmail.com>
2022-05-22 14:40:33 -0300

    20220522

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,8 +2,10 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import swaggerUI from 'swagger-ui-express';
 import createConnection from '@shared/infra/typeorm';
-
 import '@shared/container';
+
+// importar CORS antes das rotas:
+import cors from 'cors';
 
 import { router } from './routes';
 import swaggerFile from '../../../swagger.json';
@@ -27,6 +29,13 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
+/**
+ * se precisar configurar as permissões de acesso da aplicação para apenas um domínio ou ip:
+ * app.use(cors({
+ *   origin: "domain.com"
+ * }))
+ */
+app.use(cors());
 app.use(router);
 
 /** Em middlewares de erro, o parâmetro "Error" precisa ser o primeiro.
